4ed7ddf873db7b5e5a80692aeadce8fe05df64f8
ahSO2 <alyssahgithub@outlook.com>
2026-01-08 17:11:09 +0000

    Code for billinear interpolation
    For warping of image using optical flow

--- ModelDevelopment/PlumeVelocity/EvaluateOpticalFlow.py
+++ ModelDevelopment/PlumeVelocity/EvaluateOpticalFlow.py
@@ -35,13 +35,94 @@ def plot_optical_flow_Farneback(image, flow_components, n, save_loc=None):
     plt.show()
     plt.close()
 
-def warp(original, target, flow_field):
-    #Create empty array to store warped frame
+def find_dest(x, y, flow_x, flow_y):
+    return (x + flow_x, y+ flow_y)
 
-    #For each pixel in the original frame
-        #Map to its new location
-    #Use bil
-    pass
+fd_vect = np.vectorize(find_dest)
+
+def billinear_interpolate(values_array, coord):
+
+    #Pad the array with copies of the edge rows
+    padded_arr = np.pad(values_array, pad_width=1, mode="edge")
+
+    #Round the coordinate to nearest integer
+    #Coord is given in form (y, x)
+    int_coord = (int(np.round(coord[0], 0)), int(np.round(coord[1], 0)))
+
+    x = int_coord[1]
+    y = int_coord[0]
+
+    x1 = int_coord[1]
+    x2 = int_coord[1] + 1
+    y1 = int_coord[0]
+    y2 = int_coord[0] + 1
+
+    Q11 = padded_arr[x1, y1]
+    Q12 = padded_arr[x1, y2]
+    Q21 = padded_arr[x2, y1]
+    Q22 = padded_arr[x2, y2]
+
+    R1 = Q11*(x2 - x)/(x2 - x1) + Q21*(x - x1)/(x2 - x1)
+    R2 = Q12*(x2 - x)/(x2 - x1) + Q22*(x - x1)/(x2 - x1)
+
+    V = R1*(y2 - y)/(y2 - y1) + R2*(y - y1)/(y2 - y1)
+
+    return V
+
+def warp(original, next, flow_field):
+    '''Warp the second "next" image back to the original,
+    using the optical flow between the pair as the
+    inverse transform for this mapping. This allows use
+    of backward mapping, giving a better warp than forward.'''
+
+    #original = original[0:450,0:550]
+    #next = next[0:450, 0:550]
+    #flow_field = flow_field[0:450, 0:550, :]
+
+    #Create empty array to store warped frame
+    warped = np.empty_like(original)
+
+    x_pixels = np.arange(0, warped.shape[1])
+    y_pixels = np.arange(0, warped.shape[0])
+    X, Y = np.meshgrid(x_pixels, y_pixels)
+
+
+    destinations = fd_vect(X, Y, flow_field[:,:,0], flow_field[:,:,1])
+
+    #Quick check that the calculated destinations correspond to the correct flow
+    #x_dest, y_dest = fd_vect(X, Y, flow_field[:,:,0], flow_field[:,:,1])
+    #x_flow = x_dest - X
+    #y_flow = y_dest - Y
+    #components = np.empty((486, 648, 2))
+    #components[:,:,0] = x_flow
+    #components[:,:,1] = y_flow
+    #plot_optical_flow_Farneback(original, components, n=10, save_loc=None)
+
+    #For each pixel in the warped frame
+    for x in range(0, warped.shape[1]):
+        print("Warping col: " + str(x))
+        for y in range(0, warped.shape[0]):
+            #For each pixel in the original, find where in
+            #the "next" image it is mapped to by the flow
+            #Billinear interpolate the values from the next
+            #image at this point, to get the pixel val for the warped img
+
+            dest = (destinations[0][y,x], destinations[1][y,x])
+
+            if int(round(dest[0], 0)) < original.shape[0] and int(round(dest[1], 0)) < original.shape[1]:
+                value = next[int(round(dest[1], 0)), int(round(dest[0], 0))]
+
+                #value = billinear_interpolate(next, (int(round(dest[0], 0)), int(round(dest[1], 0))))
+                warped[y, x] = value
+
+
+    plt.imshow(warped, cmap='gray')
+    plt.show()
+
+
+
+
+
 
 def interpolation_error(interpolated, target):
     pass
@@ -57,7 +138,8 @@ def calc_interp_error_for_sequence(sequence, names, flow_sequence):
         flow_vals = flow_sequence[sequence_index]
         #flow_vals = np.ones((486, 648, 2)) * 5
 
-        plot_optical_flow_Farneback(current_img, flow_vals, n=10)
+        #plot_optical_flow_Farneback(current_img, flow_vals, n=10)
+        warped_image = warp(current_img, next_img, flow_vals)
 
 
 
@@ -83,3 +165,5 @@ print(sample_sequence_flow_vals.shape)
 calc_interp_error_for_sequence(sample_sequence, names, sample_sequence_flow_vals)
 
 
+#TODO Warping is giving result with the wrong orientation
+#TODO do a double check over the billinear interpolation calc
